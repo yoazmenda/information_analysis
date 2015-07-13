ce4cfa9e95443767595693d04f3bec2570bbce51
yoazmenda <yoazmenda@gmail.com>
2015-07-13 20:00:21 +0300

    added project data folder

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,17 @@ public class Main {
 
 	
 	public static void main(String argv[]){
-		System.out.println("hello from java");
+	//command line parsing
+		
+		
+		
+		
+		
 		
 	}
+		
+		
+		
+		
+		
 }
