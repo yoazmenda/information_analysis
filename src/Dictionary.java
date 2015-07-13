11c72f95f9b85894f85bdd1ed232bcd7400dd8c0
yoazmenda <yoazmenda@gmail.com>
2015-07-13 21:50:10 +0300

    new dictionary class

---
+++ src/Dictionary.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.ArrayList;
+
+
+public class Dictionary {
+
+	public Dictionary(ArrayList<File> trainingSetFiles) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import sun.org.mozilla.javascript.JavaAdapter;
+ 
 
 public class Main {
 
 	
-	public static void main(String argv[]){
+	public static void main(String argv[]) throws IOException{
 	//command line validation
 	if (argv.length != 5){
 		System.err.println("input error: usage example: tree_learn traindir/ test.examples 25 7 output.txt");
+		System.err.printf("argv length: %d\n",argv.length);
 		System.exit(-1);
 	}
 	
@@ -28,14 +36,31 @@ public class Main {
 		}
 		if(dirent.getName().contains(".labels")){
 			testLabels = dirent;
-		}		
+		}
 	}
-	System.out.println(trainingSetFiles);
-	System.out.println(testExamples);
-	System.out.println(testLabels);
-		
+	int numOfClassifications = trainingSetFiles.size();	
+	double validationPercantage = Double.parseDouble(argv[2]);	
+	int L = Integer.parseInt(argv[3]);
+	File output = new File(argv[4]);			
+	if(!output.exists()){
+		output.createNewFile();
+	}
+	FileWriter fw = new FileWriter(output.getAbsoluteFile());
+	BufferedWriter bw = new BufferedWriter(fw);
+	bw.write(new Date().toString()+"\n");
+	bw.write("Learning results:\n");
+
+	Dictionary dict = new Dictionary(trainingSetFiles);
 	
-	}
+	
+	
+	
+	
+	
+	
+	
+	bw.close();
+	}//end main
 		
 		
 		
